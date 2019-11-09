6caf1b67838c75c10abf69c0d77b14acff315940
Cristian <cristian.noop@gmail.com>
2019-11-09 16:25:17 +0200

    Toggling visibility of add courses

--- src/components/AddCourseSection.vue
+++ src/components/AddCourseSection.vue
@@ -1,19 +1,34 @@
 <template>
     <div>
-        <button id="add-course-button" class="blue-button">+</button>
-        <span id="add-course">
-                                <input class="input" type="text" placeholder="Course title" id="title">
-                                <input class="input" type="number" min="1" max="8" placeholder="Semester" id="semester">
-                                <input class="input" type="number" min="0" max="100" placeholder="Grade" id="grade">
-                                <button class="green-button" id="save-course">Save</button>
-                                <button class="grey-button" id="cancel-course">Cancel</button>
-                            </span>
+        <button id="add-course-button" class="blue-button" @click="toggleDisplay" >+</button>
+        <span id="add-course" v-bind:style="{ display: addCourse }">
+            <input class="input" type="text" placeholder="Course title" id="title">
+            <input class="input" type="number" min="1" max="8" placeholder="Semester" id="semester">
+            <input class="input" type="number" min="0" max="100" placeholder="Grade" id="grade">
+            <button class="green-button" id="save-course">Save</button>
+            <button class="grey-button" id="cancel-course">Cancel</button>
+        </span>
     </div>
 </template>
 
 <script>
     export default {
-        name: "AddCourseSection"
+        name: "AddCourseSection",
+        data: () => {
+            return {
+                addCourse: 'none',
+            }
+        },
+        methods: {
+            toggleDisplay: function () {
+                if (this.addCourse === "none") {
+                    this.addCourse = "inline";
+                }
+                else {
+                    this.addCourse = "none"
+                }
+            }
+        }
     }
 </script>
 
